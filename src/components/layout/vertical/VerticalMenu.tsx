46f507a9058b171f5b7662d281963cce8a0c9e9f
Felipe Mendes <felipe.moura@playmovel.com.br>
2025-09-15 10:02:51 -0300

    criação da tela financeiro e retirada de compra de plano do cadastro

--- src/components/layout/vertical/VerticalMenu.tsx
+++ src/components/layout/vertical/VerticalMenu.tsx
@@ -101,6 +101,16 @@ const VerticalMenu = ({ scrollMenu }: Props) => {
             Financeiro
           </MenuItem>
         </MenuSection>
+        <MenuSection label={'Utilidades'}>
+          <MenuItem href='/painel' icon={<i className='ri-tools-fill' />}>
+            Ingtegração API
+          </MenuItem>
+        </MenuSection>
+        <MenuSection label={'Gerenciamento'}>
+          <MenuItem href='/financeiro' icon={<i className='ri-money-dollar-circle-line' />}>
+            Financeiro
+          </MenuItem>
+        </MenuSection>
       </Menu>
     </ScrollWrapper>
   )
