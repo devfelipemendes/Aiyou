1caa8fa968417d8c9099365ae783af18685794e3
Guilherme Cunha <guilherme.cunha@playmovel.com.br>
2025-09-23 10:43:30 -0300

    commit

--- src/components/layout/vertical/VerticalMenu.tsx
+++ src/components/layout/vertical/VerticalMenu.tsx
@@ -90,41 +90,13 @@ const VerticalMenu = ({ scrollMenu }: Props) => {
             Operadores
           </MenuItem>
         </MenuSection>
+
         <MenuSection label={'Utilidades'}>
           <MenuItem href='/painel' icon={<i className='ri-tools-fill' />}>
             Ingtegração API
           </MenuItem>
         </MenuSection>
-        <MenuSection label={'Gerenciamento'}>
-          <MenuItem href='/financeiro' icon={<i className='ri-money-dollar-circle-line' />}>
-            Financeiro
-          </MenuItem>
-        </MenuSection>
-        <MenuSection label={'Utilidades'}>
-          <MenuItem href='/painel' icon={<i className='ri-tools-fill' />}>
-            Ingtegração API
-          </MenuItem>
-        </MenuSection>
-        <MenuSection label={'Gerenciamento'}>
-          <MenuItem href='/financeiro' icon={<i className='ri-money-dollar-circle-line' />}>
-            Financeiro
-          </MenuItem>
-        </MenuSection>
-        <MenuSection label={'Utilidades'}>
-          <MenuItem href='/painel' icon={<i className='ri-tools-fill' />}>
-            Ingtegração API
-          </MenuItem>
-        </MenuSection>
-        <MenuSection label={'Gerenciamento'}>
-          <MenuItem href='/financeiro' icon={<i className='ri-money-dollar-circle-line' />}>
-            Financeiro
-          </MenuItem>
-        </MenuSection>
-        <MenuSection label={'Utilidades'}>
-          <MenuItem href='/painel' icon={<i className='ri-tools-fill' />}>
-            Ingtegração API
-          </MenuItem>
-        </MenuSection>
+
         <MenuSection label={'Gerenciamento'}>
           <MenuItem href='/financeiro' icon={<i className='ri-money-dollar-circle-line' />}>
             Financeiro
