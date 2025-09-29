dfc3102b287b77185cf14f7373eb7d66a8dbd8fd
Felipe Mendes <felipe.moura@playmovel.com.br>
2025-09-29 17:07:03 -0300

    ajustes

--- src/app/(pages)/painel/page.tsx
+++ src/app/(pages)/painel/page.tsx
@@ -158,12 +158,12 @@ const DashboardCRM = () => {
   const apiFirstAccess = useAppSelector(selectFirstAccess)
 
   useEffect(() => {
-    if (apiFirstAccess === true) {
+    if (apiFirstAccess === false) {
       dispatch(setFirstAccess(true))
       dispatch(openModal())
     }
 
-    if (apiFirstAccess === false) {
+    if (apiFirstAccess === true) {
       dispatch(setFirstAccess(false))
     }
   }, [apiFirstAccess, dispatch])
