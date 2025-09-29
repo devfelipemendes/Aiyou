292d58e10f42c1c3ad94a9d1dbf5c4dfb8f68d5f
Felipe Mendes <felipe.moura@playmovel.com.br>
2025-09-29 17:08:27 -0300

    fixx

--- src/app/(pages)/painel/page.tsx
+++ src/app/(pages)/painel/page.tsx
@@ -147,7 +147,6 @@ const DashboardCRM = () => {
   useEffect(() => {
     if (!dataInteractions) return
     calculaAudioAndText()
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [dataInteractions])
 
   const { firstAccess } = useUserMe()
